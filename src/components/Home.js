2a2a045538147d39adf401cf7c765a9b7612e8b6
kiingdavies <daviesadedeji007@gmail.com>
2020-01-09 13:32:59 +0100

    Styled Header done

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import {
+  API_URL,
+  API_KEY,
+  API_BASE,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../config";
+
+//import components from elements
+import HeroImage from "./elements/HeroImage";
+import SearchBar from "./elements/SearchBar";
+import Grid from "./elements/Grid";
+import MovieThumb from "./elements/MovieThumb";
+import Spinner from "./elements/Spinner";
+import LoadMoreBtn from "./elements/LoadMoreBtn";
+
+//import custom hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
+
+  return (
+    <>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
+    </>
+  );
+};
+
+export default Home;
