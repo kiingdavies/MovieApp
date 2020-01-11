c9812dbb12ee33c459d162845b92fe9d0a9d27d9
kiingdavies <daviesadedeji007@gmail.com>
2020-01-11 08:28:35 +0100

    MovieThumb/InfoBar

---
+++ src/components/Movie.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+//Components
+import Navigation from "./elements/Navigation";
+import MovieInfo from "./elements/MovieInfo";
+import MovieInfoBar from "./elements/MovieInfoBar";
+import Actor from "./elements/Actor";
+import Grid from "./elements/Grid";
+import Spinner from "./elements/Spinner";
+
+import { useMovieFetch } from "./hooks/useMovieFetch";
+
+export const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+
+  return (
+    <>
+      <Navigation />
+      <MovieInfo />
+      <MovieInfoBar />
+      <Grid>
+        <Actor />
+      </Grid>
+      <Spinner />
+    </>
+  );
+};
+
+export default Movie;
